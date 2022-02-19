b11c02db5f908895b334024357e630a33d9636f8
JorianWoltjer <jorianwoltjer@hotmail.com>
2022-02-19 19:05:11 +0100

    Improve summary function, and add commenting

--- host.py
+++ host.py
@@ -5,18 +5,19 @@ import nmap
 import json
 import re
 
-# default timer per socket faster is better for scanning time
-socket.setdefaulttimeout(0.2)
-
-# Opening joson mac prefix database  
+# Load mac prefixes for vendor lookup
 with open("data/mac-prefix-table.json") as f:
     mac_prefixes = json.load(f)
 
+# Time to wait for a response from a host (default = 0.2 seconds)
+def set_timeout(timeout):
+    socket.setdefaulttimeout(timeout)
 
+# Format seperated text with color with ANSI escape codes
 def format_highlight(text, color, other_color=Fore.LIGHTBLACK_EX):
-    return re.sub(rf'(\w+)(\W*)', rf'{color}\1{other_color}\2', text) + Style.RESET_ALL   
+    return re.sub(rf'(\w+)(\W*)', rf'{color}\1{other_color}\2', text) + Style.RESET_ALL
 
-# Class for object storage per host that has been detected as up 
+# Class for storing and analysing hosts found to be up
 class Host:
     def __init__(self, ip, mac):
         self.ip = ip
@@ -25,52 +26,57 @@ class Host:
         self.hostname = None
         self.os = None
         self.ports = []
-        # self.applications
-        # self.services
-    
+
+    # Get vendor from mac address using mac-prefix-table.json
     def get_vendor(self):
         matches = list(filter(self.mac.upper().startswith, mac_prefixes))
-        if len(matches) > 0:
-            return mac_prefixes[max(matches, key=len)]  # Return longest match
-    
-    # Grabs the hostname from the found host
+        if len(matches) > 0:  # Return longest match if found
+            return mac_prefixes[max(matches, key=len)]
+
+    # Get hostname from IP using sockets
     def get_hostname(self):
         if self.hostname: return self.hostname
-        
+
         try:
             self.hostname = socket.gethostbyaddr(self.ip)[0]
         except socket.herror:  # If not found
             self.hostname = None
-        
+
         return self.hostname
-    
-    # Grabs the OS from the found host
+
+    # Get OS information using nmap
     def get_os(self):
         if self.os: return self.os
-        
+
         nm = nmap.PortScanner()
-        result = nm.scan(self.ip, ','.join(str(p) for p in self.ports), arguments="-n -O")
+        ports = ",".join(str(p) for p in self.ports)
+        result = nm.scan(self.ip, ports, arguments="-n -O")  # Arguments: -n = no DNS, -O = OS detection
         try:
             match = result['scan'][self.ip]['osmatch'][0]
-            self.os = {"name": match['name'], "accuracy": match['accuracy']}
-        except (KeyError, IndexError):
+            self.os = {"name": match['name'], "accuracy": match['accuracy']}  # Only store name and accuracy
+        except (KeyError, IndexError):  # If not found
             self.os = None
-        
+
         return self.os
 
-    # scans the ports on TCP status    
+    # Scan TCP ports on host
     def scan_ports(self, ports_to_scan):
         for port in ports_to_scan:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            result = s.connect_ex((self.ip, port))
+            result = s.connect_ex((self.ip, port))  # Try to connect to port
             s.close()
-            
-            if result == 0:
+
+            if result == 0:  # If port is open
                 self.ports.append(port)
-    
-    # scans the ports through multithreading on TCP status              
+
+    # Scan TCP ports with threading
     def scan_ports_fast(self, ports_to_scan, threads):
-        chunk_size = len(ports_to_scan) // threads + 1
+        """
+        Splits ports_to_scan into chunks, and runs scan_ports() on each chunk in a thread. 
+        Example (ports_to_scan: range(1, 1000), threads: 10):
+            Port chunks: [range(1, 100), range(100, 200), range(200, 300), ..., range(900, 1000)]
+        """
+        chunk_size = len(ports_to_scan) // threads + 1  # Size devided by number of threads, rounded up
         thread_list = []
         for i in range(threads):
             start = i * chunk_size
@@ -78,44 +84,36 @@ class Host:
             t = Thread(target=self.scan_ports, args=(ports_to_scan[start:min(end, len(ports_to_scan))],))
             thread_list.append(t)
             t.start()
-        
-        for t in thread_list:
+
+        for t in thread_list:  # Wait for all threads to finish
             t.join()
-    
-    #  summary function which captures all class object data      
-    def summary(self, ARGS):  # JSON summary
-        data = {
-            "ip": self.ip,
-            "mac": self.mac,
-            "vendor": self.vendor
-        }
-        if ARGS.hostname:
-            data["hostname"] = self.hostname
-        if ARGS.ports:
-            data["ports"] = self.ports
-        if ARGS.os:
-            data["os"] = self.os
-        
+
+    # Gives a list of all attributes of the host (used for JSON)
+    def summary(self, ARGS):
+        data = self.__dict__
+        data = {k: v for k, v in data.items() if k in ["ip", "mac", "vendor"] or ARGS.__dict__[k]}  # Filter out not scanned attributes
         return data
-    
-    # constructor object for the network scan
+
+    # Get formatted string of host information
     def __str__(self):
         result = ""
-        attributes = {
+        attributes = {  # Attributes displayed below host
             "Hostname": self.hostname,
             "Operating System": f"{self.os['name']} {Fore.LIGHTWHITE_EX}({self.os['accuracy']}%)" if self.os else None,
             "Ports": format_highlight(', '.join(str(p) for p in self.ports), Fore.LIGHTBLUE_EX) if self.ports else None,
         }
-        vendor_str = " => " + self.vendor if self.vendor else ""
+        vendor_str = " => " + self.vendor if self.vendor else ""  # Add vendor if found (with arrow)
         ip = format_highlight(self.ip, Fore.LIGHTWHITE_EX)
         mac = format_highlight(self.mac, Fore.YELLOW)
-        
+
+        # Main host information
         result += f"{Fore.GREEN}⬤ {Style.RESET_ALL} Host {Style.BRIGHT}{ip}{Style.RESET_ALL} is up " + " "*(15-len(self.ip)) + \
             f"{Fore.LIGHTBLACK_EX}({Fore.YELLOW}{mac}{Style.RESET_ALL}{vendor_str}{Fore.LIGHTBLACK_EX}){Style.RESET_ALL}"
-        
+
         # Filter out empty attributes
         attributes = {k: v for k, v in attributes.items() if v}
-        
+
+        # Attributes below host, found during scan
         items = list(attributes.items())
         for attribute, value in items:
             if (attribute, value) != items[-1]:
--- main.py
+++ main.py
@@ -1,64 +1,68 @@
 import argparse
 import sys
 
+# Parse arguments, before loading any other modules
 parser = argparse.ArgumentParser()
-parser.add_argument("network", help="'auto' for automatic discovery, subnet, or single host (ex. 192.168.1.0/24 or 192.168.1.42)")
+parser.add_argument("network", help="'auto' for automatic detection, subnet, or single host (ex. 192.168.1.0/24 or 192.168.1.42)")
+parser.add_argument("-a", "--all", help="enable all options, try to find everything", action="store_true")
 parser.add_argument('-p', '--ports', required="--os" in " ".join(sys.argv), help="ports to scan for all found hosts. 'top', 'all', a range (ex. 1-1024), or a comma-separated list (ex. 22,80,443)")
 parser.add_argument('-o', '--output', help="save output to file in JSON format", type=argparse.FileType('w', encoding='UTF-8'))
 parser.add_argument('-n', '--hostname', help="get hostname for all found hosts", action='store_true')
-parser.add_argument('-t', '--threads', help="number of threads to use for port scanning", type=int, default=10)
-parser.add_argument('--os', help="scan for operating system on all found hosts", action="store_true")
+parser.add_argument('-t', '--timeout', help="timeout for socket connections (in seconds)", type=float, default=0.2)
+parser.add_argument('-T', '--threads', help="number of threads to use for port scanning", type=int, default=10)
+parser.add_argument('--os', help="scan for operating system on all found hosts (requires ports)", action="store_true")
 parser.add_argument('--json', help="output to stdout in JSON format", action="store_true")
 ARGS = parser.parse_args()
 
-from scapy.all import srp, ARP, Ether
-from colorama import Fore, Style
-from pyfiglet import print_figlet
-from threading import Thread
-import datetime
+from scapy.all import srp, ARP, Ether  # Sending ARP requests
+from colorama import Fore, Style  # Printing colors
+from pyfiglet import print_figlet  # Printing ascii art header
+from threading import Thread  # Threading to improve performance
+import datetime 
 import time
 import json
 import re
 
-from host import Host
+from host import Host, set_timeout
 
+# Overwrite print function, to not print during JSON output
 def print_(*args, **kwargs):
     if not ARGS.json:
         print(*args, **kwargs)
 
-# Header
-if not ARGS.json: print_figlet("Scanmap", font="slant", colors="LIGHT_RED")
-print_("© 2022 Jorian Woltjer & Giovanni Aramu - All rights reserved")
-print_()
-
+# Convert string to list of ports
 def parse_ports(s):
-    if s == "all":
+    if s == "all":  # All ports
         return range(1, 65536)
-    elif s == "top":
+    elif s == "top":  # Top 100 ports
         with open("data/tcp-top100.txt") as f:
             return [int(port) for port in f.read().splitlines()]
-    elif re.match("\d+-\d+", s):
+    elif re.fullmatch("\d+-\d+", s):  # Range of ports
         a, b = s.split("-")
         return range(int(a), int(b) + 1)
-    elif re.match("\d+(,\d+)*", s):
+    elif re.fullmatch("\d+(,\d+)*", s):  # Comma-separated list of ports
         return [int(port) for port in s.split(",")]
+    else:
+        parser.error(f"Invalid --ports range or list: {s}")
 
+# Convert ip and subnet mask to CIDR notation
 def to_subnet(ip, mask):
     import ipaddress
     
-    cidr = sum([bin(int(x)).count('1') for x in mask.split('.')])
-    return str(ipaddress.ip_network(f"{ip}/{cidr}", strict=False))
+    cidr = sum([bin(int(x)).count('1') for x in mask.split('.')])  # Count bits
+    return str(ipaddress.ip_network(f"{ip}/{cidr}", strict=False))  # Convert to network range
 
+# Automatically find subnet from interfaces
 def auto_get_subnet():
     import netifaces
 
     gateway = netifaces.gateways()['default'][netifaces.AF_INET]
-
     interface = netifaces.ifaddresses(gateway[1])
     info = interface[netifaces.AF_INET][0]
 
     return to_subnet(info['addr'], info['netmask'])
 
+# Run function for each host in a thread
 def do_all_threaded(target, *args):
     threads = []
     for host in up_hosts:
@@ -66,18 +70,29 @@ def do_all_threaded(target, *args):
         threads.append(t)
         t.start()
     
-    for t in threads:
+    for t in threads:  # Wait for all threads to finish
         t.join()
 
 # Initialize
 start_time = time.time()
 up_hosts = []
+set_timeout(ARGS.timeout)
 
-if ARGS.ports:
+# Parse arguments
+if ARGS.ports:  # Parse string to list of ports
     ARGS.ports = parse_ports(ARGS.ports)
-    
-if ARGS.network == "auto":
+if ARGS.network == "auto":  # Automatically find subnet
     ARGS.network = auto_get_subnet()
+if ARGS.all:  # Enable all options
+    if not ARGS.ports:
+        ARGS.ports = parse_ports("top")
+    ARGS.os = True
+    ARGS.hostname = True
+
+# Header
+if not ARGS.json: print_figlet("Scanmap", font="slant", colors="LIGHT_RED")
+print_("© 2022 Jorian Woltjer & Giovanni Aramu - All rights reserved")
+print_()
 
 # Start info
 current_time = datetime.datetime.now()
@@ -89,7 +104,6 @@ print_(Fore.LIGHTBLACK_EX, end="")
 answered, unanswered = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ARGS.network), timeout=2, verbose=(not ARGS.json))
 print_(Style.RESET_ALL, end="")
 
-# Save hosts
 for host in answered.res:
     up_hosts.append(Host(host[1].psrc, host[1].hwsrc))
 
@@ -108,13 +122,13 @@ if ARGS.os:
     print_(f"{Fore.LIGHTBLACK_EX}Detecting operating systems...{Style.RESET_ALL}")  # TODO: Progress bar, or estimate
     do_all_threaded(Host.get_os)
 
-# Results
+# Print results
 print_(f"{Style.BRIGHT}Results:{Style.RESET_ALL}")
 for host in up_hosts:
     print_(host)
 print_()
 
-# Statistics
+# Print statistics
 total = len(answered) + len(unanswered)
 print_(f"Found {Style.BRIGHT}{len(up_hosts)}{Style.RESET_ALL} out of {Fore.LIGHTWHITE_EX}{total}{Style.RESET_ALL} hosts are up")
 
@@ -123,7 +137,7 @@ print_(f"Finished in {Fore.LIGHTWHITE_EX}{duration}{Style.RESET_ALL} seconds")
 
 # JSON output
 def to_json():
-    output = {
+    output = {  # Main options
         "network": ARGS.network,
         "start_time": int(current_time.timestamp()),
         "duration": duration,
@@ -132,15 +146,15 @@ def to_json():
     }
     
     for host in up_hosts:
-        data = host.summary(ARGS)
+        data = host.summary(ARGS)  # Get summary of attributes for all hosts
         output["up_hosts"].append(data)
     
     return json.dumps(output, indent=4)
 
-if ARGS.output:
+if ARGS.output:  # Output to file
     print_(f"Results saved to {Fore.LIGHTWHITE_EX}{ARGS.output.name}{Style.RESET_ALL}")
     with ARGS.output as f:
         f.write(to_json())
         
-if ARGS.json:
+if ARGS.json:  # Output to stdout
     print(to_json())
